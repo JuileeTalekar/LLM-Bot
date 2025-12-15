c3b249d648cdd126a91dbd79b06c4df80102cbfa
Juilee Talekar <juileetalekar@gmail.com>
2025-12-16 03:07:58 +0530

    Structuring Components

---
+++ src/components/ChatComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const ChatComponent: React.FunctionComponent = () => {
+
+  return (
+     <div className="flex flex-col h-[80vh] bg-white"></div>
+  );
+
+  
+};
+
+export default ChatComponent;
